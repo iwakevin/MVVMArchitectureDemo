efd41115b05df8a926169c413cef14b501035338
gonghonglou <gonghonglou@gmail.com>
2018-02-12 16:42:37 +0800

    Initial Commit

---
+++ MVVMArchitectureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMArchitectureDemo
+//
+//  Created by gonghonglou on 2018/2/12.
+//  Copyright © 2018年 Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
