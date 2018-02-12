ed7a15335f69b2d0e33a847548dedf2836c5a924
gonghonglou <gonghonglou@gmail.com>
2018-02-12 17:11:05 +0800

    Add Home

---
+++ MVVMArchitectureDemo/Module/Home/ViewContainer/MAHomeViewContainer.m
@@ -0,0 +1,21 @@
+//
+//  MAHomeViewContainer.m
+//  MVVMArchitectureDemo
+//
+//  Created by gonghonglou on 2018/2/12.
+//  Copyright © 2018年 Troy. All rights reserved.
+//
+
+#import "MAHomeViewContainer.h"
+
+@implementation MAHomeViewContainer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ MVVMArchitectureDemo/Module/Home/ViewModel/MAHomeViewModel.m
@@ -0,0 +1,13 @@
+//
+//  MAHomeViewModel.m
+//  MVVMArchitectureDemo
+//
+//  Created by gonghonglou on 2018/2/12.
+//  Copyright © 2018年 Troy. All rights reserved.
+//
+
+#import "MAHomeViewModel.h"
+
+@implementation MAHomeViewModel
+
+@end
